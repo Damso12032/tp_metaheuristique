d671e14666d1529f0d3f0a0a9ea2207cc84ce9ab
Damso12032 <Adamasall232@gamil.com>
2023-04-28 19:54:48 +0200

    fin tp

---
+++ descent_est_spt.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1,2,3,4],[56,54,42,41],label="descent_est_lrpt sur aaa3")
+plt.xlabel("nombre d'iterations")
+plt.ylabel("makespan")
+plt.legend()
+plt.grid()
+plt.show()
